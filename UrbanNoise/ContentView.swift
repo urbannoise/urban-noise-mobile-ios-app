b1f50f5c7e7eaca0b9d12d18aa3b957bc5a6c67c
Madalin Valceleanu <devmobile@vmadalin.com>
2019-11-02 13:04:36 +0100

    init commit

---
+++ UrbanNoise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UrbanNoise
+//
+//  Created by Madalin Valceleanu on 02/11/2019.
+//  Copyright © 2019 Urban Noise. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UrbanNoiseTests/UrbanNoiseTests.swift
@@ -0,0 +1,34 @@
+//
+//  UrbanNoiseTests.swift
+//  UrbanNoiseTests
+//
+//  Created by Madalin Valceleanu on 02/11/2019.
+//  Copyright © 2019 Urban Noise. All rights reserved.
+//
+
+import XCTest
+@testable import UrbanNoise
+
+class UrbanNoiseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
